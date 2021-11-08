803bdf14c4458243fd290913b3b80f48ef43ad33
Aryamann <aryamanmehra98@gmail.com>
2021-11-07 21:40:02 -0500

    Updated each individual item to save qty

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 
 
-  function myFunction() {
-    var x = document.querySelector(".line_text");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+
+
+function myFunction() {
+  var x = document.querySelector(".line_text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
+
+function totalCost() {
+  var x = document.getElementById("quantity").value;
+  var num = (x * y).toFixed(2);
+  document.getElementById("total").innerHTML = "Total cost will be $" + num + " for " + x + " item(s).";
+  localStorage.setItem(key, x)
+}
+
+
+
+function resubmit() {
+
+  var x = localStorage.getItem(key);
+  document.getElementById("quantity").value = x
+  console.log("z: " + x)
+  var num = (x * y).toFixed(2);
+  if (x == null) {
+    document.getElementById("total").innerHTML = "";
+  }
+  else {
+    document.getElementById("total").innerHTML = "Total cost will be $" + num + " for " + x + " item(s).";
+  }
+
+}
