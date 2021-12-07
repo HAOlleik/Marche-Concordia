56f3156f92ea28352e149bd4125294721ef13318
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2021-12-07 16:09:07 -0500

    Update page_8.php

--- page_8.php
+++ page_8.php
@@ -1,4 +1,5 @@
 <!--Charles Eimer 26747310 Edit product page   -->
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,12 +15,12 @@
 <body>
     <!-- Navigation for the Backpages Charles 26747310  -->
     <div class="nav_back">
-        <a id="welcome" href="page9.php"><?php print "Welcome " . $_SESSION['username']; ?></a>
-        <a href="/index.html">Home</a>
-        <a href="/final_b.html">Product List</a>
-        <a href="/p11.html">Order List</a>
-        <a href="pagenine.html">User List</a>
-        <a href="/index.html">Sign out</a>
+        <a id="welcome" href="final_b.php"><?php print "Welcome " . $_SESSION['username']; ?></a>
+        <a href="/index.php">Home</a>
+        <a href="/final_b.php">Product List</a>
+        <a href="/p11.php">Order List</a>
+        <a href="pagenine.php">User List</a>
+        <a href="/index.php">Sign out</a>
     </div>
     <section class="outer_edit">
         <!--Inner container for the edit  -->
